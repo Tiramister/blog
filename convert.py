b97e26eac88d2328928266b8e5ca82a57302c6d6
Tiramister <nh111424@gmail.com>
2022-08-24 21:26:45 +0900

    fix(convert.py): 精度向上

--- convert.py
+++ convert.py
@@ -15,48 +15,81 @@ def findall(pattern: str):
     return re.findall(pattern, text, flags=re.DOTALL)
 
 
-def sub(pattern: str, repl: str):
+def sub_all(pattern: str, repl: str):
     global text
     text = re.sub(pattern, repl, text, flags=re.DOTALL)
 
 
-# タイトル
-title: str = search('\A# (.*?)\n').group(1)
-sub('\A# .*?\n', '')
+def sub_once(pattern: str, repl: str):
+    global text
+    text = re.sub(pattern, repl, text, count=1, flags=re.DOTALL)
+
+
+# タイトル退避
+pattern = '\A# (.*?)\n'
+title: str = search(pattern).group(1)
+sub_all(pattern, '')
 
 # タグ削除
-sub('###### tags: .*?\n', '')
+pattern = '###### tags: .*?\n'
+sub_all(pattern, '')
 
 # ソースコードを退避
-for (id, (language, code)) in enumerate(findall(r'```(.*?)=\n(.*?)```')):
-    filename: str = f'{id}.{language}'
+code_id = 0
+pattern = r'```(.*?)=\n(.*?)```'
+while True:
+    match = search(pattern)
+    if match == None:
+        break
+
+    (language, code) = match.groups()
+    if language == '':
+        language = 'txt'
+
+    filename = f'{code_id}.{language}'
+    sub_once(
+        pattern, '{{' + f'<code file="{filename}" language="{language}">' + '}}')
     with open(filename, 'w') as f:
         f.write(code)
-sub(r'```(.*?)```', r'<code>')
+
+# 複数行の数式を退避
+eqs: list[str] = []
+pattern = r'(\$\$.*?\$\$)'
+while True:
+    eq = search(pattern)
+    if eq == None:
+        break
+    sub_once(pattern, f' <math-{len(eqs)}> ')
+    eqs.append(eq.group(0))
+
+# インライン数式を退避
+pattern = r'(\$.*?\$)'
+while True:
+    eq = search(pattern)
+    if eq == None:
+        break
+    sub_once(pattern, f' <math-{len(eqs)}> ')
+    eqs.append(eq.group(0))
 
 # リンクを退避
-links: list[tuple[str, str]] = findall(r'\[([^\[\]]*?)\]\((http.*?)\)')
-sub(r'\[[^\[\]]*?\]\(http.*?\)', '')
+pattern = r'\[([^\[\]]*?)\]\((http.*?)\)'
+links: list[tuple[str, str]] = findall(pattern)
+sub_all(pattern, '')
 
-# 前後スペース
-sub(r'(\$.+?\$)', r' \1 ')
-sub(r'(\*.+?\*)', r' \1 ')
-sub(r'(\*\*.+?\*\*)', r' \1 ')
-
-# 数式内の特殊文字にバックスラッシュ
-sub(r'([\\_\'\^\{\}])', r'\\\1')
-
-# shortcode を戻す
-sub(r'<code>', r'{{<code file="main.cpp" language="cpp">}}')
+# 数式を戻す
+pattern = r'([\\_\'\^\{\}])'
+for (id, eq) in enumerate(eqs):
+    # 記号をエスケープ
+    eq = re.sub(pattern, r'\\\1', eq)
+    sub_all(f'<math-{id}>', eq)
 
 print(r'''---
-title: {}
-date: 
+title: "{}"
+date:
 tags: [atcoder, abc]
 links:'''.format(title))
 for (label, url) in links:
-    print(r'''  - label: {}
-    url: {}'''.format(label, url))
+    print(r'''  - label: "{}"
+    url: "{}"'''.format(label, url))
 print("---")
-
 print(text)
