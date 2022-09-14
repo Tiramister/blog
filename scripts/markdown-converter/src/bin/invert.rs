f74e02783cf307e8ea897c8c181039fcc1629906
Tiramister <nh111424@gmail.com>
2022-09-14 22:09:03 +0900

    入力

--- scripts/markdown-converter/src/bin/invert.rs
+++ scripts/markdown-converter/src/bin/invert.rs
@@ -1,3 +1,18 @@
-fn main() {
+use anyhow::Result;
+use std::io::{self, Read};
+
+fn read_file<R: Read>(reader: &mut R) -> Result<String> {
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() -> Result<()> {
     println!("invert to valid markdown style");
+
+    let mut stdin = io::stdin();
+    let text = read_file(&mut stdin)?;
+    println!("{}", text);
+
+    Ok(())
 }
