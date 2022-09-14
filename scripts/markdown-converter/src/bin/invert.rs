94e3b3ddc12644c7b00a5b1caa6656e6bd896e72
Tiramister <nh111424@gmail.com>
2022-09-14 22:11:51 +0900

    front matter の抽出

--- scripts/markdown-converter/src/bin/invert.rs
+++ scripts/markdown-converter/src/bin/invert.rs
@@ -1,5 +1,21 @@
 use anyhow::Result;
+use regex::Regex;
 use std::io::{self, Read};
+
+fn get_front_matter(text: &mut String) -> String {
+    let re = Regex::new(r"(?s)^---\n(.*?)\n---").unwrap();
+
+    let cap_opt = re.captures(text);
+    let front_matter = if let Some(cap) = cap_opt {
+        cap.get(1).unwrap().as_str().to_string()
+    } else {
+        "".to_string()
+    };
+
+    *text = re.replace(text, "").to_string();
+
+    front_matter
+}
 
 fn read_file<R: Read>(reader: &mut R) -> Result<String> {
     let mut buf = String::new();
@@ -11,8 +27,12 @@ fn main() -> Result<()> {
     println!("invert to valid markdown style");
 
     let mut stdin = io::stdin();
-    let text = read_file(&mut stdin)?;
-    println!("{}", text);
+    let mut text = read_file(&mut stdin)?;
+
+    let front_matter = get_front_matter(&mut text);
+    println!("front matter: \n{}", front_matter);
+    println!("~~~~~~~~~~");
+    println!("text: \n{}", text);
 
     Ok(())
 }
