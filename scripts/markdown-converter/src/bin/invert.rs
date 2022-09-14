fcaaabf42ca5ee75108c32f4143f3cd2ab01c2e4
Tiramister <nh111424@gmail.com>
2022-09-14 23:00:04 +0900

    FrontMatter 構造体

--- scripts/markdown-converter/src/bin/invert.rs
+++ scripts/markdown-converter/src/bin/invert.rs
@@ -2,6 +2,44 @@ use anyhow::Result;
 use regex::Regex;
 use std::io::{self, Read};
 
+struct Link {
+    label: String,
+    url: String,
+}
+
+struct FrontMatter {
+    title: Option<String>,
+    date: Option<String>,
+    tags: Vec<String>,
+    links: Vec<Link>,
+}
+
+impl FrontMatter {
+    fn to_string(&self) -> String {
+        let mut texts = Vec::new();
+
+        if let Some(title) = &self.title {
+            texts.push(format!("# {}", title));
+        }
+        if let Some(date) = &self.date {
+            texts.push(format!("<!--date {}-->", date));
+        }
+        if !self.tags.is_empty() {
+            texts.push(format!("<!--tags {}-->", self.tags.join(" ")));
+        }
+        if !self.links.is_empty() {
+            for link in &self.links {
+                texts.push(format!(
+                    "<!--link label=\"{}\" url={}-->",
+                    link.label, link.url
+                ));
+            }
+        }
+
+        texts.join("\n")
+    }
+}
+
 fn get_front_matter(text: &mut String) -> String {
     let re = Regex::new(r"(?s)^---\n(.*?)\n---").unwrap();
 
@@ -33,6 +71,19 @@ fn main() -> Result<()> {
     println!("front matter: \n{}", front_matter);
     println!("~~~~~~~~~~");
     println!("text: \n{}", text);
+    println!("~~~~~~~~~~");
+
+    let front_matter = FrontMatter {
+        title: Some("test title".to_string()),
+        date: Some("2022-09-14".to_string()),
+        tags: vec!["atcoder".to_string()],
+        links: vec![Link {
+            label: "Problem".to_string(),
+            url: "https://atcoder.jp/contests/abc001/tasks/abc001_1".to_string(),
+        }],
+    };
+
+    println!("front matter: \n{}", front_matter.to_string());
 
     Ok(())
 }
